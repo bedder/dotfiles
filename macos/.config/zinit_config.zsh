70fd0b5578d92bde644dbdd3a6102a8e7243ed4a
Matthew Bedder <matthew@bedder.co.uk>
2025-12-19 13:00:11 +0000

    Adjust zinit plugins

--- macos/.config/zinit_config.zsh
+++ macos/.config/zinit_config.zsh
@@ -1,16 +1,27 @@
-zinit ice depth=1 proto=ssh; zinit light romkatv/powerlevel10k
-zinit ice depth=1 proto=ssh; zinit light zsh-users/zsh-syntax-higlighting
-zinit ice depth=1 proto=ssh; zinit light zsh-users/zsh-syntax-completions
-zinit ice depth=1 proto=ssh; zinit light zsh-users/zsh-syntax-autosuggestions
+# zinit extensions
+zinit ice depth=1 proto=ssh; zinit light romkatv/powerlevel10k              # Base theme
+zinit ice depth=1 proto=ssh; zinit light zsh-users/zsh-syntax-highlighting  # Syntax highlighting
+zinit ice depth=1 proto=ssh; zinit light zsh-users/zsh-completions          # Additional completions
+zinit ice depth=1 proto=ssh; zinit light zsh-users/zsh-autosuggestions      # Additional suggestions
 
-zinit snippet OMZL::git.zsh
-zinit snippet OMZP::git
-zinit snippet OMZP::sudo
-zinit snippet OMZP::aws
-zinit snippet OMZP::kubectl
-zinit snippet OMZP::kubectx
-zinit snippet OMZP::command-not-found
+# oh-my-zsh basic plugins
+zinit snippet OMZL::git.zsh            # Base git support
+zinit snippet OMZP::command-not-found  # Hint towards solutions to issues
+
+# oh-my-zsh application specific plugins
+if [ -x "$(command -v kubectl)" ]; then
+    zinit snippet OMZP::kubectl  # Adds kubectl autocompletion
+    zinit snippet OMZP::kubectx  # Allows k8s context in prompts
+fi
+if [ -x "$(command -v poetry)" ]; then
+    zinit snippet OMZP::poetry   # Adds poetry autocompletion
+fi
+if [ -x "$(command -v uv)" ]; then
+    zinit snippet OMZP::uv       # Adds uv autocompletion
+fi
 
 # Load completions
-autoload -Uz compinit && compinit
-zinit cdreplay -q
+autoload -Uz compinit && compinit && zinit cdreplay -q
+
+# Allow word deletion to be path aware
+autoload -U select-word-style && select-word-style bash
